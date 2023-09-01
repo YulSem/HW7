3ca34fb46f5f6f9249139607adbe7df9a0f2ea7c
Yuliya <you@example.com>
2023-09-01 11:40:05 +0300

    Задача 52

--- Program.cs
+++ Program.cs
@@ -29,13 +29,50 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
-Console.WriteLine("Введите количество строк");
-int m = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите количество столбцов");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix = new int [m,n];
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] matrix = new int [m,n];
+
+// for (int i=0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j=0; j<matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j]=new Random().Next(0,20);
+//         Console.Write(matrix[i,j]+" ");
+//     }
+//     Console.WriteLine();
+// }
+//  Console.WriteLine();
+
+//  Console.WriteLine("Введите позицию элемента");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  int b = Convert.ToInt32(Console.ReadLine());
+//  if (a>m || b>n)
+//  Console.WriteLine("Такого числа нет");
+//  else
+//  {
+//  Console.WriteLine("Значение искомого элемента "+matrix[a,b]);
+//  }
+
+//  Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите первую размерность");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую размерность");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int [rows, columns];
 
 for (int i=0; i<matrix.GetLength(0); i++)
 {
@@ -44,16 +81,22 @@ for (int i=0; i<matrix.GetLength(0); i++)
         matrix[i,j]=new Random().Next(0,20);
         Console.Write(matrix[i,j]+" ");
     }
-    Console.WriteLine();
+     Console.WriteLine();
 }
- Console.WriteLine();
-
- Console.WriteLine("Введите позицию элемента");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m || b>n)
- Console.WriteLine("Такого числа нет");
- else
- {
- Console.WriteLine("Значение искомого элемента "+matrix[a,b]);
- }
+double[] averages = new double[columns];
+for (int col = 0; col < columns; col++)
+        {
+            double sum = 0;
+            
+            for (int row = 0; row < rows; row++)
+            {
+                sum += matrix[row, col];
+            }
+            
+            averages[col] = sum / rows;
+        }
+        
+        for (int col = 0; col < columns; col++)
+        {
+            Console.WriteLine($"Среднее арифметическое столбца {col + 1}: {averages[col]}");
+        }
